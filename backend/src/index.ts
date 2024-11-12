4c5f8b0b98631375b719ad98be4ab8316fd8ef78
Michael Chuang <zhaungsont@gmail.com>
2024-11-12 22:23:19 +0800

    feat: setup FE & BE project env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,27 @@
-const message: string = 'Hello TypeScript!';
-console.log(message);
+import express from 'express';
+import { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.DEBUG);
+
+// Basic GET endpoint
+app.get('/health', (req: Request, res: Response) => {
+	res.json({ message: 'OK' });
+});
+
+app.post('/post', (req: Request, res: Response) => {
+	console.log('POST request received', req.body);
+	res.json({ message: 'OK' });
+});
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
